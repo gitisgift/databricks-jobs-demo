1ef947dc6712caa98d109a3ac6b5258d70939c18
zubair <zubair@zubairs-MacBook-Air.local>
2026-02-04 12:24:37 +0530

    added tests

--- src/jobs/entrypoint.py
+++ src/jobs/entrypoint.py
@@ -1,17 +1,17 @@
-import argparse
-from jobs.logic import run_job
+# import argparse
+# from jobs.logic import run_job
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--catalog", required=True)
-    parser.add_argument("--source", required=True)
-    parser.add_argument("--limit", type=int, default=10)
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--catalog", required=True)
+#     parser.add_argument("--source", required=True)
+#     parser.add_argument("--limit", type=int, default=10)
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    run_job(
-        catalog=args.catalog,
-        source=args.source,
-        limit=args.limit
-    )
+#     run_job(
+#         catalog=args.catalog,
+#         source=args.source,
+#         limit=args.limit
+#     )
